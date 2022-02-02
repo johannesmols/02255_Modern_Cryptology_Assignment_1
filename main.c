534a68f8642f695cd09429f849a1b0f9e65a8352
Johannes Mols <johannes.mols@googlemail.com>
2022-02-02 22:12:06 +0100

    Implemented ShiftRows

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-#include "aes_s_boxes.c"
+#include "constants.c"
 
-void print_box(const unsigned char box[]) {
+#pragma region Printing
+
+void print_box(const unsigned char* box) {
     for (int i = 0; i < 16; i++) {
-        printf("%x", box[i]);
+        printf("%02x", box[i]);
         if ((i + 1) % 4 == 0) {
             printf("\n");
         } else {
@@ -14,11 +16,39 @@ void print_box(const unsigned char box[]) {
     printf("\n");
 }
 
-void sub_bytes(unsigned char v[], const unsigned char s_box[]) {
+#pragma endregion Printing
+
+#pragma region Array operations
+
+void sub_bytes(unsigned char* arr, const unsigned char* s_box) {
     for (int i = 0; i < 16; i++) {
-        v[i] = s_box[v[i]];
+        arr[i] = s_box[arr[i]];
     }
 }
+
+void swap(unsigned char* arr, int i1, int i2) {
+    unsigned char tmp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = tmp;
+}
+
+void shift_left(unsigned char* arr, int start, int end, int n) {
+    for (int iter = 0; iter < n; iter++) {
+        unsigned char first = arr[start];
+        for (int i = start; i < end; i++) {
+            swap(arr, i, i + 1);
+        }
+        arr[end] = first;
+    }
+}
+
+void shift_rows(unsigned char* arr) {
+    for (int r = 0; r < 4; r++) {
+        shift_left(arr, r * 4, r * 4 + 3, r);
+    }
+}
+
+#pragma endregion array operations
 
 int main()
 {
@@ -26,8 +56,20 @@ int main()
                                   0x3d, 0xf4, 0xc6, 0xf8,
                                   0xe3, 0xe2, 0x8d, 0x48,
                                   0xbe, 0x2b, 0x2a, 0x08};
+
+    printf("Original:\n");
     print_box(test_sub_1);
+
     sub_bytes(test_sub_1, SBox);
+    printf("After SubBytes:\n");
+    print_box(test_sub_1);
+
+    /*sub_bytes(test_sub_1, InverseSBox);
+    printf("After Inverse SubBytes:\n");
+    print_box(test_sub_1);*/
+
+    printf("After Shift Rows:\n");
+    shift_rows(test_sub_1);
     print_box(test_sub_1);
 
     return 0;
